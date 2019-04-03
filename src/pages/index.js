5426a0d27b714089dfcf59db7f62f9ebc055e00a
M Miller <miller.2676@gmail.com>
2019-04-03 15:39:09 -0700

    re-test build

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
+import Layout from "../components/layout"
 
 class IndexPage extends React.Component {
   render() {
     const pages = this.props.data.allNodePage.edges
     const pageTitles = pages.map(page => <li>{page.node.title}</li>)
-    return <ul>{pageTitles}</ul>
+    return (
+      <Layout>
+        <ul>{pageTitles}</ul>
+      </Layout>
+    )
   }
 }
 
