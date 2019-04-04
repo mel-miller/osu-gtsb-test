928a2e40b741ca2d7c8086b6783a2f612c7499cd
M Miller <miller.2676@gmail.com>
2019-04-04 12:21:46 -0700

    reorg for more examples

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,16 @@
 import React from "react"
+import { Link } from "gatsby"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { graphql } from "gatsby"
 
-class IndexPage extends React.Component {
-  render() {
-    const node5 = this.props.data.nodePage
-    return (
-      <Layout>
-        <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-        <div>
-          <h2>{node5.title}</h2>
-          <div dangerouslySetInnerHTML={{ __html: node5.body.value }} />
-        </div>
-      </Layout>
-    )
-  }
-}
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <Link to="/single-node/">Go to single node example page.</Link>
+  </Layout>
+)
 
 export default IndexPage
-
-export const query = graphql`
-  query pageQuery {
-    nodePage(drupal_internal__nid: { eq: 3 }) {
-      drupal_internal__nid
-      title
-      body {
-        value
-      }
-    }
-  }
-`
